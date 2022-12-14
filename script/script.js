5e8206c11cdc94b19a05691c73786e43c4aa8337
ivanwlam <yayitsivan+github@hotmail.com>
2022-12-14 14:21:27 -0800

    Feature: Update Permissions functionality

--- script/script.js
+++ script/script.js
@@ -1,37 +1,24 @@
-	const vv_cMin = 60 * 24 * 7;
-	const redirectUrl = "/wordpress.php/";
-	
-	const vv_setC_unhide = function () {
-		console.log("temp: func: set and unhiding");
-		vv_setC(vv_cMin);
-		vv_unhide();
-	};
-	
-	const vv_unhide = function () {
-		console.log('temp: unhide content');
-	};
-	
-// 	const vv_checkParams = function () {
-// 		console.log('temp: check params');
-// 		return false;
-// 	};
-	
-// 	const vv_checkC = function () {
-// 		console.log('temp: check cookies');
-// 		return false;
-// 	};
-	
-	const vv_redirect = function () {
-		if (window.location.href.includes(redirectUrl)) return;
-		window.location.replace(redirectUrl);
-	}
+const vv_cMin = 60 * 24 * 7;
+const redirectUrl = "/wordpress.php/";
 
-	const getDaysFromMinutes = function (minutes) {
-	  let date = new Date();
-	  date.setTime(date.getTime() + (minutes * 60 * 1000));
-	  // console.log("date", date);
-	  return date;
-	};
+/*
+const vv_setC_unhide = function () {
+  console.log("temp: func: set and unhiding");
+  vv_setC(vv_cMin);
+  vv_unhide();
+};
+
+const vv_unhide = function () {
+  console.log('temp: unhide content');
+};
+*/
+
+const getDaysFromMinutes = function (minutes) {
+  let date = new Date();
+  date.setTime(date.getTime() + (minutes * 60 * 1000));
+  // console.log("date", date);
+  return date;
+};
   
 const getValidVisitorCookieString = function (minutes) {
     // Let cookie apply across pages in domain: Add "domain=.example.com;path=/": https://stackoverflow.com/questions/5671451/creating-a-javascript-cookie-on-a-domain-and-reading-it-across-sub-domains
@@ -40,12 +27,21 @@ const getValidVisitorCookieString = function (minutes) {
 	  let cookieValue = "vv=true;expires=" + expiryDate.toUTCString() + ";domain=.ivanwlam.com;path=/;secure";
 	  console.log("cookieValue", cookieValue);
 	  return cookieValue;
-	};
+};
 
-	const vv_setC = function (minutes) {
-    document.cookie = getValidVisitorCookieString(minutes);
-    console.log("Running: vv_setC");
-	};
+const setBodyAsNotPermitted = function () {
+  document.body.classList.add("permission-denied");
+}
+
+const vv_redirect = function () {
+  if (window.location.href.includes(redirectUrl)) return;
+  window.location.replace(redirectUrl);
+}
+
+const vv_setC = function (minutes) {
+  document.cookie = getValidVisitorCookieString(minutes);
+  console.log("Running: vv_setC");
+};
  
 // setValidVisitorCookie(cookieMinutes);
 	
